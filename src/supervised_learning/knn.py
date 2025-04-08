7d52cc341865a8cc981318e17067c3d4e3b2f7b3
gaeng02 <sondin144@gmail.com>
2025-04-08 23:57:31 +0900

    feat : knn

--- src/supervised_learning/knn.py
+++ src/supervised_learning/knn.py
@@ -1,1 +1,13 @@
+import numpy as np
+from src.distance import Euclidean
 
+class KNNClassifier :
+
+    def __init__ (self, k = 3) :
+        self.k = k
+    
+    def fit (self, X, y) :
+        self.X_train = X
+        self.y_train = y
+
+    
