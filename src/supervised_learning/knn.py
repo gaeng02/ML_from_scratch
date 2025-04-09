dc087147216322d3473e8db931184b596d99fdc2
gaeng02 <sondin144@gmail.com>
2025-04-09 22:00:33 +0900

    feat : knn

--- src/supervised_learning/knn.py
+++ src/supervised_learning/knn.py
@@ -10,4 +10,24 @@ class KNNClassifier :
         self.X_train = X
         self.y_train = y
 
-    
+    def predict (self, X) :
+        predictions = []
+
+        for x in X :
+            distances = []
+
+            for x_train in self.X_train :
+                distances.append(Euclidean(x, x_train))
+
+            k_indixes = np.argsort(distances)[ : self.k]
+            
+            k_nearest_labels = []
+            for index in k_indices :
+                k_nearest_labels.append(self.y_train[i])
+
+            labels, counts = np.unique(k_nearest_labels, return_counts = True)
+            majority_label = labels[np.argmax(counts)]
+
+            predictions.append(majority_label)
+
+        return np.array(predictions)
