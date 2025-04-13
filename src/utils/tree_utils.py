9385821c6bbbc91f8ad31051da9022131d579623
gaeng02 <sondin144@gmail.com>
2025-04-13 23:00:32 +0900

    issue : how to measure information_gain

--- src/utils/tree_utils.py
+++ src/utils/tree_utils.py
@@ -42,4 +42,25 @@ def gini_index (y) :
 
     return gini
 
+def information_gain (y, y_left, y_right, criterion = "entropy") :
+    '''
+    Calculate the information gain as a potential split
 
+    Args :
+        y (np.ndarray) : parent node
+        y_left (np.ndarray) : left child labels
+        y_right (np.ndarray) : right child labels
+        criterion (str) : "entropy" or "gini_index" (default = "entropy")
+
+    Returns :
+        float : information gain
+    '''
+
+    if (criterion == "entropy") : measure = entropy
+    elif (criterion == "gini") : measure = gini_index
+    else :
+        raise ValueError("")
+
+    # how to measure?
+    
+    return gain
